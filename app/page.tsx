e4076f38a491370155901609f082619d4f12c588
Alabere-coder <alabere4real247@gmail.com>
2025-12-31 03:32:01 +0100

    updating the page

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   const { hasLoaded, markLoaded } = useAppLoaded()
 
   const handleLoadingComplete = () => {
-    setIsLoading(true);
+    setIsLoading(false);
     markLoaded()
   };
 
